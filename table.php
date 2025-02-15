93d4952b94b2ceda58014ca2109dadba70952164
aska20 <128310987+aska20@users.noreply.github.com>
2025-02-15 15:06:24 +0545

    Add files via upload

---
+++ table.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Flexbox Layout</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="tableStyle.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <header>header</header>
+        <nav>nav</nav>
+        <div class="content">
+            <div class="main">main</div>
+            <div class="sidebar">sidebar</div>
+        </div>
+        <footer>footer</footer>
+    </div>
+</body>
+
+</html>
